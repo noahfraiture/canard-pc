0f775d55134edf83149fd2722b02deec165072d1
NoahFraiture <noah.fraiture@gmail.com>
2023-05-01 23:04:33 +0200

    start signin, not working at all

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'color_theme.dart';
 import 'custom_color.dart';
 import 'shop_page.dart';
 import 'library_page.dart';
 import 'logout_page.dart';
+import 'sign_in.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -62,12 +68,25 @@ class Main extends StatefulWidget {
 class _MainState extends State<Main> {
   int _currentIndex = 0;
 
+  final _children = const [
+    ShopPage(),
+    LibraryPage(),
+    LogoutPage(),
+    SignInPage(),
+  ];
+  final _bottomItems = const [
+    BottomNavigationBarItem(activeIcon: Icon(Icons.home), icon: Icon(Icons.home_outlined), label: 'Accueil'),
+    BottomNavigationBarItem(activeIcon: Icon(Icons.bookmark), icon: Icon(Icons.bookmark_border), label: 'Bibliothèque'),
+    BottomNavigationBarItem(activeIcon: Icon(Icons.person), icon: Icon(Icons.person_outline), label: 'Profil'),
+    BottomNavigationBarItem(activeIcon: Icon(Icons.login), icon: Icon(Icons.login_outlined), label: 'Connexion'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(
         index: _currentIndex,
-        children: const [ShopPage(), LibraryPage(), LogoutPage()],
+        children: _children,
       ),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -85,11 +104,7 @@ class _MainState extends State<Main> {
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentIndex,
         onTap: (int index) => setState(() => _currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(activeIcon: Icon(Icons.home), icon: Icon(Icons.home_outlined), label: 'Accueil'),
-          BottomNavigationBarItem(activeIcon: Icon(Icons.bookmark), icon: Icon(Icons.bookmark_border), label: 'Bibliothèque'),
-          BottomNavigationBarItem(activeIcon: Icon(Icons.person), icon: Icon(Icons.person_outline), label: 'Profil'),
-        ],
+        items: _bottomItems,
         selectedItemColor: Theme.of(context).colorScheme.onSurface,
         unselectedItemColor: Theme.of(context).colorScheme.onSurface,
         backgroundColor: Theme.of(context).colorScheme.surface,
